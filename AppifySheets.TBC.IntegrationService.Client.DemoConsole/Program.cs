e8ca57a6b4eb8581e704e6c7247b3e6e9860f03c
Petre <petre.chitashvili@appifysheets.com>
2023-11-17 16:36:26 +0400

    Updated Demo code (in Program.cs)

--- AppifySheets.TBC.IntegrationService.Client.DemoConsole/Program.cs
+++ AppifySheets.TBC.IntegrationService.Client.DemoConsole/Program.cs
@@ -2,6 +2,7 @@
 using AppifySheets.Immutable.BankIntegrationTypes;
 using AppifySheets.TBC.IntegrationService.Client.ApiConfiguration;
 using AppifySheets.TBC.IntegrationService.Client.SoapInfrastructure.GetAccountMovements;
+using AppifySheets.TBC.IntegrationService.Client.SoapInfrastructure.GetPaymentOrderStatus;
 using AppifySheets.TBC.IntegrationService.Client.SoapInfrastructure.ImportSinglePaymentOrders;
 using AppifySheets.TBC.IntegrationService.Client.TBC_Services;
 
@@ -10,6 +11,10 @@ var tbcSoapCaller = new TBCSoapCaller("certificate.pfx", "CertificatePassword", 
 
 var accountMovements = await Worker.GetDeserialized(new GetAccountMovementsDeserializer(tbcSoapCaller,
     new Period(new DateTime(2023, 9, 1), new DateTime(2023, 9, 26))));
+
+var checkStatus = await Worker
+    .GetDeserialized(new GetPaymentOrderStatusDeserializer(tbcSoapCaller,
+        new SoapGetPaymentOrderStatus(1632027071)));
 
 var withinBankGel = await Worker
     .GetDeserialized(new ImportSinglePaymentOrdersDeserializer(tbcSoapCaller,
@@ -68,6 +73,21 @@ var toAnotherBankCurrencyBad = await Worker
                 BeneficiaryName = "TEST",
                 SenderAccountWithCurrency = BankAccountWithCurrencyV.Create(new BankAccountV("GE20TB7467945067800004"), CurrencyV.USD).Value
             })));
+
+var toChina = await Worker
+    .GetDeserialized(new ImportSinglePaymentOrdersDeserializer(tbcSoapCaller,
+        new SoapImportSinglePaymentOrders(
+            new TransferToOtherBankForeignCurrencyPaymentOrderIo("", "China",
+                // "ICBKCNBJSZN", "INDUSTRIAL AND COMMERCIAL BANK OF CHINA SHENZHEN BRANCH", "SHA", "Invoice(LZSK202311028)",
+                "ICBKCNBJSZN", "INDUSTRIAL AND COMMERCIAL BANK OF CHINA SHENZHEN BRANCH", "SHA", "(Invoice/Amount/Currency): (LZSK202311028/2590.00/USD) (LZSK202311027/2510.00/USD)",
+                BankAccountWithCurrencyV.Create(new BankAccountV("4000109819100186641"), CurrencyV.USD).Value)
+            {
+                DocumentNumber = 123,
+                Amount = 5100m,
+                BeneficiaryName = "Shenzhen Shinekoo Supply Chain Co.,Ltd",
+                SenderAccountWithCurrency = BankAccountWithCurrencyV.Create(new BankAccountV("GE20TB7467945067800004"), CurrencyV.USD).Value
+            })));
+
 
 var toTreasury = await Worker
     .GetDeserialized(new ImportSinglePaymentOrdersDeserializer(tbcSoapCaller,
